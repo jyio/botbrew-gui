c0bedeea5bbd5ed06d16990c41946fed83a09c02
Jiang Yio <inportb@gmail.com>
2012-06-08 17:47:22 -0400

    do not namespace/clone; just MNT_DETACH for easy cleanup

--- jni/init/init.c
+++ jni/init/init.c
@@ -16,18 +16,13 @@
 #include <sys/syscall.h>
 #include <sys/prctl.h>
 #include <sys/wait.h>
+#include <linux/fs.h>
 #include <linux/loop.h>
 
 #include "strnstr.h"
 
 #define ENV_PATH	"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/botbrew/bin:/usr/lib/busybox"
 #define LOOP_MAX	4096
-
-struct config {
-	char *target;
-	char *cwd;
-	char *const *argv;
-};
 
 struct mountspec {
 	const char *src;
@@ -221,30 +216,24 @@ static void mount_setup(char *target, int loopdev) {
 	fs = strconcat(target,"/run/lock");
 	mkdir(fs,01777);
 	free(fs);
+	size_t target_len = strlen(target);
+	int i;
+	struct stat st;
+	for(i = 0; i < n_foreign_mounts; i++) {
+		struct mountspec m = foreign_mounts[i];
+		if((m.src)&&(stat(m.src,&st) != 0)) continue;
+		size_t dst_len = strlen(m.dst);
+		char *dst = (char*)malloc(target_len+dst_len+1);
+		memcpy(dst,target,target_len);
+		memcpy(dst+target_len,m.dst,dst_len+1);	// includes null terminator
+		mkdir(dst,0755);
+		if(mount(m.src,dst,m.type,m.flags,m.data)) rmdir(dst);
+		else if(m.remount_flags) mount(dst,dst,NULL,m.remount_flags|MS_REMOUNT,NULL);
+		free(dst);
+	}
 }
 
 static void mount_teardown(char *target, int loopdev) {
-	char *fs = strconcat(target,"/botbrew/etc");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/botbrew/home");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/botbrew/root");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/botbrew/var");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/botbrew/share");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/botbrew/run");
-	umount2(fs,MNT_DETACH);
-	free(fs);
-	fs = strconcat(target,"/run");
-	umount2(fs,MNT_DETACH);
-	free(fs);
 	if(loopdev) loopdev_umount2(target,MNT_DETACH);
 	else umount2(target,MNT_DETACH);
 }
@@ -262,78 +251,7 @@ static int copy(char *src, char *dst) {
 	return 0;
 }
 
-static int main_clone(struct config *config) {
-	char cwd[PATH_MAX];
-	if(getcwd(cwd,sizeof(cwd)) == NULL) {
-		fprintf(stderr,"whoops: cannot get working directory\n");
-		return EXIT_FAILURE;
-	}
-	if(chdir(config->target)) {
-		fprintf(stderr,"whoops: cannot chdir to namespace\n");
-		return EXIT_FAILURE;
-	}
-	size_t target_len = strlen(config->target);
-	int i;
-	struct stat st;
-	for(i = 0; i < n_foreign_mounts; i++) {
-		struct mountspec m = foreign_mounts[i];
-		if((m.src)&&(stat(m.src,&st) != 0)) continue;
-		size_t dst_len = strlen(m.dst);
-		char *dst = (char*)malloc(target_len+dst_len+1);
-		memcpy(dst,config->target,target_len);
-		memcpy(dst+target_len,m.dst,dst_len+1);	// includes null terminator
-		mkdir(dst,0755);
-		if(mount(m.src,dst,m.type,m.flags,m.data)) rmdir(dst);
-		else if(m.remount_flags) mount(dst,dst,NULL,m.remount_flags|MS_REMOUNT,NULL);
-		free(dst);
-	}
-	if(chroot(".")) {
-		fprintf(stderr,"whoops: cannot chroot\n");
-		return EXIT_FAILURE;
-	}
-	if((chdir(cwd))&&(chdir("/"))) {
-		fprintf(stderr,"whoops: cannot chdir to chroot\n");
-		return EXIT_FAILURE;
-	}
-	privdrop();
-	char *env_path = getenv("PATH");
-	if((env_path)&&(env_path[0])) {
-		size_t env_path_len = strlen(env_path);
-		char *newpath = (char*)malloc(sizeof(ENV_PATH)+env_path_len);
-		char *append = strstr(env_path,"::");
-		if(append) {	// break string at :: and insert
-			append++;
-			size_t prepend_len = append-env_path;
-			memcpy(newpath,env_path,prepend_len);
-			memcpy(newpath+prepend_len,ENV_PATH,sizeof(ENV_PATH)-1);
-			memcpy(newpath+prepend_len+sizeof(ENV_PATH)-1,append,env_path_len-prepend_len+1);
-		} else {
-			memcpy(newpath,ENV_PATH":",sizeof(ENV_PATH":")-1);
-			memcpy(newpath+sizeof(ENV_PATH":")-1,env_path,env_path_len+1);	// includes null terminator
-		}
-		setenv("PATH",newpath,1);
-		free(newpath);
-	} else setenv("PATH",ENV_PATH":/android/sbin:/system/sbin:/system/bin:/system/xbin",1);
-	unsetenv("LD_LIBRARY_PATH");
-	setenv("BOTBREW_PREFIX",config->target,1);
-	if(config->argv == NULL) {
-		const char *argv0[2];
-		argv0[0] = "/init.sh";
-		argv0[1] = 0;
-		config->argv = (char**)&argv0;
-	}
-	if(execvp(config->argv[0],config->argv)) {
-		int i = 1;
-		fprintf(stderr,"whoops: cannot run `%s",config->argv[0]);
-		while(config->argv[i]) fprintf(stderr," %s",config->argv[i++]);
-		fprintf(stderr,"'\n");
-		return errno;
-	}
-	return 0;	// wtf
-}
-
 int main(int argc, char *argv[]) {
-	struct config config;
 	struct stat st;
 	char apath[PATH_MAX];
 	int remount = 0;
@@ -342,7 +260,7 @@ int main(int argc, char *argv[]) {
 	char *self = argv[0];
 	uid_t uid = getuid();
 	// get absolute path
-	config.target = realpath(dirname(self),apath);
+	char *child_root = realpath(dirname(self),apath);
 	int c;
 	while(1) {
 		static struct option long_options[] = {
@@ -363,7 +281,7 @@ int main(int argc, char *argv[]) {
 					fprintf(stderr,"whoops: --dir is only available for uid=0\n");
 					return EXIT_FAILURE;
 				}
-				config.target = realpath(optarg,apath);
+				child_root = realpath(optarg,apath);
 				break;
 			case 'r':
 				remount = 1;
@@ -374,7 +292,7 @@ int main(int argc, char *argv[]) {
 				usage(self);
 		}
 	}
-	config.argv = (optind==argc)?NULL:(argv+optind);
+	char *const *child_argv = (optind==argc)?NULL:(argv+optind);
 	// prevent privilege escalation: fail if link/symlink is not owned by superuser
 	if(uid) {
 		if(lstat(self,&st)) {
@@ -387,30 +305,30 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	// check if target exists
-	if(stat(config.target,&st)) {
-		fprintf(stderr,"whoops: `%s' does not exist\n",config.target);
+	if(stat(child_root,&st)) {
+		fprintf(stderr,"whoops: `%s' does not exist\n",child_root);
 		return EXIT_FAILURE;
 	}
 	if(S_ISREG(st.st_mode)) {
-		loopmount = config.target;
-		config.target = (char*)malloc(PATH_MAX);
-		config.target = realpath(dirname(loopmount),config.target);
-		config.target = (char*)realloc(config.target,strlen(config.target)+1);
+		loopmount = child_root;
+		child_root = (char*)malloc(PATH_MAX);
+		child_root = realpath(dirname(loopmount),child_root);
+		child_root = (char*)realloc(child_root,strlen(child_root)+1);
 	} else if(!S_ISDIR(st.st_mode)) {
-		fprintf(stderr,"whoops: `%s' is not a directory\n",config.target);
+		fprintf(stderr,"whoops: `%s' is not a directory\n",child_root);
 		return EXIT_FAILURE;
 	} else {
-		if((st.st_uid)||(st.st_gid)) chown(config.target,0,0);
-		if((st.st_mode&S_IWGRP)||(st.st_mode&S_IWOTH)) chmod(config.target,0755);
+		if((st.st_uid)||(st.st_gid)) chown(child_root,0,0);
+		if((st.st_mode&S_IWGRP)||(st.st_mode&S_IWOTH)) chmod(child_root,0755);
 	}
-	self = (char*)malloc(snprintf(NULL,0,"%s/init",config.target)+1);
-	sprintf(self,"%s/init",config.target);
+	self = (char*)malloc(snprintf(NULL,0,"%s/init",child_root)+1);
+	sprintf(self,"%s/init",child_root);
 	// check if directory mounted
 	int mounted = 0;
 	int loopmounted = 0;
 	FILE *fp1;
 	if(fp1 = fopen("/proc/self/mounts","r")) {
-		char *mntpt = config.target;
+		char *mntpt = child_root;
 		struct mntent *mnt;
 		while(mnt = getmntent(fp1)) {
 			if(strcmp(mnt->mnt_dir,mntpt) != 0) continue;
@@ -441,7 +359,7 @@ int main(int argc, char *argv[]) {
 			fprintf(stderr,"whoops: superuser privileges required to unmount\n");
 			return EXIT_FAILURE;
 		}
-		mount_teardown(config.target,loopmounted);
+		mount_teardown(child_root,loopmounted);
 		if(remount) mounted = 0;
 		else return EXIT_SUCCESS;
 	}
@@ -451,13 +369,13 @@ int main(int argc, char *argv[]) {
 			return EXIT_FAILURE;
 		}
 		if(loopmount) {
-			if(loopdev_mount(loopmount,config.target,"ext4",0,NULL)) {
+			if(loopdev_mount(loopmount,child_root,"ext4",0,NULL)) {
 				fprintf(stderr,"whoops: cannot mount `%s'\n",loopmount);
 				return EXIT_FAILURE;
 			}
 			loopmounted = 1;
 		}
-		mount_setup(config.target,loopmounted);
+		mount_setup(child_root,loopmounted);
 		if(strcmp(argv[0],self) != 0) {
 			time_t mtime = stat(argv[0],&st)?0:st.st_mtime;
 			if(!stat(self,&st)) {
@@ -470,47 +388,61 @@ int main(int argc, char *argv[]) {
 			if((st.st_mode&S_IWGRP)||(st.st_mode&S_IWOTH)||!(st.st_mode&S_ISUID)) chmod(self,04755);
 		}
 	}
-	// clone with new namespace
-	long stacksz = sysconf(_SC_PAGESIZE);
-	void *stack = (char*)alloca(stacksz)+stacksz;
-#ifdef __i386__
-	pid_t pid = syscall(__NR_clone,SIGCHLD|CLONE_NEWNS|CLONE_FILES,stack);
-	if(pid == 0) return main_clone(&config);
-#else
-	pid_t pid = clone((int (*)(void*))main_clone,stack,SIGCHLD|CLONE_NEWNS|CLONE_FILES,(void*)&config);
-#endif
-	if(pid < 0) {
-		fprintf(stderr,"whoops: cannot clone\n");
+	// do the chroot and chdir dance
+	char cwd[PATH_MAX];
+	if(getcwd(cwd,sizeof(cwd)) == NULL) {
+		fprintf(stderr,"whoops: cannot get working directory\n");
 		return EXIT_FAILURE;
-	} else {
-		char *pr_name = (char*)malloc(snprintf(NULL,0,"BotBrew(%d)",pid)+1);
-		sprintf(pr_name,"BotBrew(%d)",pid);
-		prctl(PR_SET_NAME,pr_name);
-		free(pr_name);
-		struct sigaction act;
-		int ret;
-		privdrop();
-		memset(&act,0,sizeof(act));
-		act.sa_handler = sighandler;
-		child_pid = pid;
-		sigaction(SIGABRT,&act,0);
-		sigaction(SIGALRM,&act,0);
-		sigaction(SIGHUP,&act,0);
-		sigaction(SIGINT,&act,0);
-		sigaction(SIGQUIT,&act,0);
-		sigaction(SIGTERM,&act,0);
-		sigaction(SIGUSR1,&act,0);
-		sigaction(SIGUSR2,&act,0);
-		sigaction(SIGCONT,&act,0);
-		sigaction(SIGSTOP,&act,0);
-		sigaction(SIGTSTP,&act,0);
-		sigaction(SIGPOLL,&act,0);
-		sigaction(SIGPROF,&act,0);
-		sigaction(SIGURG,&act,0);
-		sigaction(SIGVTALRM,&act,0);
-		sigaction(SIGXCPU,&act,0);
-		while((waitpid(pid,&ret,0)<0)&&(errno == EINTR));
-		child_pid = 0;
-		return WIFEXITED(ret)?WEXITSTATUS(ret):EXIT_FAILURE;
 	}
+	if(chdir(child_root)) {
+		fprintf(stderr,"whoops: cannot chdir to namespace\n");
+		return EXIT_FAILURE;
+	}
+	if(chroot(".")) {
+		fprintf(stderr,"whoops: cannot chroot\n");
+		return EXIT_FAILURE;
+	}
+	if((chdir(cwd))&&(chdir("/"))) {
+		fprintf(stderr,"whoops: cannot chdir to chroot\n");
+		return EXIT_FAILURE;
+	}
+	// drop privileges
+	privdrop();
+	// configure environment
+	char *env_path = getenv("PATH");
+	if((env_path)&&(env_path[0])) {
+		size_t env_path_len = strlen(env_path);
+		char *newpath = (char*)malloc(sizeof(ENV_PATH)+env_path_len);
+		char *append = strstr(env_path,"::");
+		if(append) {	// break string at :: and insert
+			append++;
+			size_t prepend_len = append-env_path;
+			memcpy(newpath,env_path,prepend_len);
+			memcpy(newpath+prepend_len,ENV_PATH,sizeof(ENV_PATH)-1);
+			memcpy(newpath+prepend_len+sizeof(ENV_PATH)-1,append,env_path_len-prepend_len+1);
+		} else {
+			memcpy(newpath,ENV_PATH":",sizeof(ENV_PATH":")-1);
+			memcpy(newpath+sizeof(ENV_PATH":")-1,env_path,env_path_len+1);	// includes null terminator
+		}
+		setenv("PATH",newpath,1);
+		free(newpath);
+	} else setenv("PATH",ENV_PATH":/android/sbin:/system/sbin:/system/bin:/system/xbin",1);
+	unsetenv("LD_LIBRARY_PATH");
+	setenv("BOTBREW_PREFIX",child_root,1);
+	// run specified command or /init.sh
+	if(child_argv == NULL) {
+		const char *argv0[2];
+		argv0[0] = "/init.sh";
+		argv0[1] = 0;
+		child_argv = (char**)&argv0;
+	}
+	if(execvp(child_argv[0],child_argv)) {
+		int i = 1;
+		fprintf(stderr,"whoops: cannot run `%s",child_argv[0]);
+		while(child_argv[i]) fprintf(stderr," %s",child_argv[i++]);
+		fprintf(stderr,"'\n");
+		return errno;
+	}
+	// wtf
+	return EXIT_FAILURE;
 }
